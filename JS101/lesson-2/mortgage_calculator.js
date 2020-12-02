cfea8549e22df876a1c956c2fc5880387b45b53e
brendan.vonick <brendan.vonick@moo.com>
2020-12-02 00:10:51 -0700

    adding lesson 2 up through the mortgage calculator program

---
+++ JS101/lesson-2/mortgage_calculator.js
@@ -0,0 +1,62 @@
+// Mortgage Calculator Program
+
+const readline = require('readline-sync');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trim() === '' ||
+         Number(number) < 0 ||
+         Number.isNaN(Number(number));
+}
+
+prompt('Welcome to the mortgage calculator!\n');
+
+while (true) {
+  prompt('Please enter the loan amount.');
+  let loanAmount = readline.question();
+
+  while (invalidNumber(loanAmount)) {
+    prompt('Please enter a positive number as the total loan amount.');
+    loanAmount = readline.question();
+  }
+
+
+  prompt('What is the annual percentage rate, as a percent greater than 0.');
+  let apr = readline.question();
+
+  while (invalidNumber(apr)) {
+    prompt('Please enter a positive number as the annual percentage rate');
+    apr = readline.question();
+  }
+
+
+  prompt('Please enter the loan duration, in years.');
+  let loanDurationYears = readline.question();
+
+  while (invalidNumber(loanDurationYears)) {
+    prompt('Please enter a positive number as the loan duration in years.');
+    loanDurationYears = readline.question();
+  }
+
+  let loanDurationMonths = loanDurationYears * 12;
+  let monthlyRate = (Number(apr) / 100) / 12;
+
+  let monthlyPayment = loanAmount *
+      (monthlyRate / (1 - Math.pow((1 + monthlyRate), (-loanDurationMonths))));
+
+  console.log(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
+
+  prompt('Would you like to perform another calculation (y/n)?');
+  let response = (readline.question()).toLowerCase();
+  while (response[0] !== 'y' && response[0] !== 'n') {
+    prompt('please enter "y" for yes, or "n" for no.');
+    response = (readline.question()).toLowerCase();
+  }
+
+  if (response === 'n') {
+    break;
+  }
+}
---
+++ JS101/lesson-2/pseudocode-practice.js
@@ -0,0 +1,58 @@
+/*
+A function that returns the sum of 2 numbers
+
+START
+
+# Given 2 numbers
+
+SET number1 = value of first number
+SET number2 = value of second number
+
+SET result = number1 + number2
+
+PRINT result
+
+END
+*/
+
+
+
+/*
+A function that takes an array of strings, and returns a string that is all
+those strings concatenated together.
+
+START
+
+# Given an array of strings
+
+SET finalString = ""
+
+SET finalString = array's elements joined with ' '
+PRINT finalString
+
+END
+*/
+
+
+/*
+a function that takes an array of integers, and returns a new array with every
+other element.
+
+START
+
+# Given an array of integers
+
+SET newArray = []
+
+SET iterator = 1
+SET savedNumber = value within array of integers at space 1
+
+WHILE iterator < length of array of integers
+  SET currentNumber = element within array of integers at space 'iterator'
+  IF iterator % 2 === 0
+    push element to the newArray
+
+PRINT newArray
+
+END
+*/
