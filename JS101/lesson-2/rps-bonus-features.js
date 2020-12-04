e5ab49e9b8c5a30786cd93865803dba138aac18a
brendan.vonick <brendan.vonick@moo.com>
2020-12-04 01:48:23 -0700

    adding rock_paper_scissors.js and rps-bonus-features.js

---
+++ JS101/lesson-2/rps-bonus-features.js
@@ -0,0 +1,74 @@
+
+const readline = require('readline-sync');
+const CHOICE_OPTIONS = ['r for rock', 'p for paper', 'sc for scissors',
+  'l for lizard', 'sp for spock'];
+const VALID_CHOICES = ['r', 'p', 'sc', 'l', 'sp'];
+let wins = 0;
+let computerWins = 0;
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function minimizeChoice(selection) {
+  let smallChoice = '';
+  if (selection.substring(0, 1).includes('r')) {
+    smallChoice = 'r';
+  } else if (selection.substring(0, 1).includes('p')) {
+    smallChoice = 'p';
+  } else if (selection.substring(0, 2).includes('sc')) {
+    smallChoice = 'sc';
+  } else if (selection.substring(0, 1).includes('l')) {
+    smallChoice = 'l';
+  } else if (selection.substring(0, 2).includes('sp')) {
+    smallChoice = 'sp';
+  }
+  return smallChoice;
+}
+
+function playerWins(choice, computerChoice) {
+  return ((choice === 'r' && computerChoice === 'sc') ||
+          (choice === 'r' && computerChoice === 'l') ||
+          (choice === 'p' && computerChoice === 'r') ||
+          (choice === 'p' && computerChoice === 'sp') ||
+          (choice === 'sc' && computerChoice === 'p') ||
+          (choice === 'sc' && computerChoice === 'l') ||
+          (choice === 'l' && computerChoice === 'p') ||
+          (choice === 'l' && computerChoice === 'sp') ||
+          (choice === 'sp' && computerChoice === 'sc') ||
+          (choice === 'sp' && computerChoice === 'r'));
+}
+
+function displayWinner(choice, computerChoice) {
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
+  if (playerWins(choice, computerChoice)) {
+    wins += 1;
+    prompt(`You win!\n=> You: ${wins}, Computer: ${computerWins}`);
+  } else if (choice === computerChoice) {
+    prompt(`It's a tie.\n=> You: ${wins}, Computer: ${computerWins}`);
+  } else {
+    computerWins += 1;
+    prompt(`Computer wins!\n=> You: ${wins}, Computer: ${computerWins}`);
+  }
+}
+
+while ((wins < 5) && (computerWins < 5)) {
+  prompt(`Choose one: ${CHOICE_OPTIONS.join(', ')}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice.substring(0, 1)) &&
+         !VALID_CHOICES.includes(choice.substring(0, 2))) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  displayWinner(minimizeChoice(choice), computerChoice);
+}
+
+prompt('');
+prompt(`Final Score! You: ${wins}, Computer: ${computerWins}`);
+prompt('Thank you for playing.');
