7f0fbacbfdde05def99b6eee083ada499551fa63
brendan.vonick <brendan.vonick@moo.com>
2020-12-08 09:40:38 -0700

    adding updated to mortgage-calculator per the code review

--- JS101/lesson-2/mortgage_calculator/mortgage_calculator.js
+++ JS101/lesson-2/mortgage_calculator/mortgage_calculator.js
@@ -1,6 +1,10 @@
 // Mortgage Calculator Program
 
 const readline = require('readline-sync');
+let loanAmount = '';
+let apr = '';
+let loanDurationYears = '';
+let response = '';
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -8,36 +12,62 @@ function prompt(message) {
 
 function invalidNumber(number) {
   return number.trim() === '' ||
-         Number(number) < 0 ||
+         Number(number) <= 0 ||
          Number.isNaN(Number(number));
 }
 
-prompt('Welcome to the mortgage calculator!\n');
+function invalidAnswer(answer) {
+  return answer.trim() !== 'yes' && answer.trim() !== 'no';
+}
 
-while (true) {
+function promptLoanAmount() {
   prompt('Please enter the loan amount.');
-  let loanAmount = readline.question();
+  loanAmount = readline.question();
 
   while (invalidNumber(loanAmount)) {
     prompt('Please enter a positive number as the total loan amount.');
     loanAmount = readline.question();
   }
+}
 
+function promptAPR() {
   prompt('What is the annual percentage rate, as a percent greater than 0.');
-  let apr = readline.question();
+  apr = readline.question();
 
   while (invalidNumber(apr)) {
     prompt('Please enter a positive number as the annual percentage rate');
     apr = readline.question();
   }
+}
 
+function promptLoanDuration() {
   prompt('Please enter the loan duration, in years.');
-  let loanDurationYears = readline.question();
+  loanDurationYears = readline.question();
 
   while (invalidNumber(loanDurationYears)) {
     prompt('Please enter a positive number for the loan duration in years.');
     loanDurationYears = readline.question();
   }
+}
+
+function promptAnotherCalculation() {
+  prompt('Would you like to perform another calculation (yes/no)?');
+  response = (readline.question()).toLowerCase();
+
+  while (invalidAnswer(response)) {
+    prompt('please enter "yes" for yes, or "no" for no.');
+    response = (readline.question()).toLowerCase();
+  }
+}
+
+prompt('Welcome to the mortgage calculator!\n');
+
+while (true) {
+  promptLoanAmount();
+
+  promptAPR();
+
+  promptLoanDuration();
 
   let loanDurationMonths = Number(loanDurationYears) * 12;
   let monthlyRate = (Number(apr) / 100) / 12;
@@ -47,14 +77,9 @@ while (true) {
 
   console.log(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
 
-  prompt('Would you like to perform another calculation (y/n)?');
-  let response = (readline.question()).toLowerCase();
-  while (response[0] !== 'y' && response[0] !== 'n') {
-    prompt('please enter "y" for yes, or "n" for no.');
-    response = (readline.question()).toLowerCase();
-  }
+  promptAnotherCalculation();
 
-  if (response === 'n') {
+  if (response === 'no') {
     break;
   }
 }
