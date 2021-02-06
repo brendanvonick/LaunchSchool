6d715e65d281a3e66980d5a74528ba645454f460
brendanvonick <bvonick92@gmail.com>
2021-02-06 00:48:32 -0700

    adding oo-rps updates

--- JS120/lesson-1/oo-rps.js
+++ JS120/lesson-1/oo-rps.js
@@ -1,6 +1,6 @@
 // RPS is a 2-player game where each player chooses one of three possible moved:
-// rock, paper, or scissors. The winner is chosen by comparing their moves with the
-// following rules:
+// rock, paper, or scissors. The winner is chosen by comparing their moves with
+// the following rules:
 //   - Rock crushes scissors, i.e. rock wins against scissors.
 //   - Scissors cuts paper, i.e., scissors beats paper.
 //   - Paper wraps wrock, i.e., paper beats rock.
@@ -19,62 +19,71 @@
 
 const readline = require('readline-sync');
 
+function createComputer() {
+  let playerObject = createPlayer();
 
-function createPlayer(playerType) {
-  return {
-    playerType: playerType,
-    move: null,
-
+  let computerObject = {
     choose() {
-      if (this.isHuman()) {
-        let choice;
+      const choices = ['rock', 'paper', 'scissors'];
+      let randomIndex = Math.floor(Math.random() * choices.length);
+      this.move = choices[randomIndex];
+    },
+  };
 
-        while (true) {
-          console.log('Please choose rock, paper, or scissors:');
-          choice = readline.question();
-          if (['rock', 'paper', 'scissors'].includes(choice)) break;
-          console.log('Sorry, invalid choice.');
-        }
+  return Object.assign(playerObject, computerObject);
+}
 
-        this.move = choice;
-      } else {
-        const choices = ['rock', 'paper', 'scissors'];
-        let randomIndex = Math.floor(Math.random() * choices.length);
-        this.move = choices[randomIndex];
+function createHuman() {
+  let playerObject = createPlayer();
+
+  let humanObject = {
+    choose() {
+      let choice;
+
+      while (true) {
+        console.log('Please choose rock, paper, or scissors:');
+        choice = readline.question();
+        if (['rock', 'paper', 'scissors'].includes(choice)) break;
+        console.log('Sorry, invalid choice.');
       }
-    },
 
-    isHuman() {
-      return this.playerType === 'human';
+      this.move = choice;
     },
   };
+
+  return Object.assign(playerObject, humanObject);
 }
 
-
-function createMove() {
+function createPlayer() {
   return {
-    // possible state: type of move (paper, rock, scissors)
-  };
-
-}
-
-
-function createRule() {
-  return {
-    // possible state? not clear whether Rules need state
+    move: null,
   };
 }
+
+// function createMove() {
+//   return {
+//     // possible state: type of move (paper, rock, scissors)
+//   };
+//
+// }
+//
+//
+// function createRule() {
+//   return {
+//     // possible state? not clear whether Rules need state
+//   };
+// }
 
 
 //  Since we don't yet know where to put 'compare', let's define it as an
 //  ordinary function.
-let compare = function(move1, move2) {
-  // not yet implemented
-}
+// let compare = function(move1, move2) {
+//   // not yet implemented
+// }
 
 const RPSGame = {
-  human: createPlayer('human'),
-  computer: createPlayer('computer'),
+  human: createHuman(),
+  computer: createComputer(),
 
   displayWelcomeMessage() {
     console.log('Welcome to Rock, Paper Scissors!');
